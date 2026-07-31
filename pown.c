2e8c1e749a825971e08fd6a1b8e0b8a86e11569e
Nabiha Tahsin <nabihatahsin688@gmail.com>
2026-07-31 20:13:04 +0600

    Refactor code structure for improved readability and maintainability

---
+++ pown.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int x,y;
+    printf("Enter x= ");
+    scanf("%d",&x);
+    printf("Enter y= ");
+    scanf("%d",&y);
+    double result=pow(x,y);
+    printf("%.2lf", result);
+    return 0;
+}
