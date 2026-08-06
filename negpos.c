e28dfb42f74dd3db9dad28cb700cc02fc7d963be
Nabiha Tahsin <nabihatahsin688@gmail.com>
2026-08-06 17:48:43 +0600

    Refactor code structure for improved readability and maintainability

---
+++ negpos.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter any numbers: ");
+    scanf("%d", &num);
+    if(num>0)
+        printf("The number is positive.");
+    else if(num<0)
+        printf("The number is negative.");
+    else
+        printf("The number is zero.");
+    return 0;
+}
