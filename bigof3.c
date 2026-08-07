0500cf355dcb5a9418d2cb07b0879b60374c6b5c
Nabiha Tahsin <nabihatahsin688@gmail.com>
2026-08-07 17:56:34 +0600

    Refactor code structure for improved readability and maintainability

---
+++ bigof3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,num3;
+    printf("Enter 3 numbers: ");
+    scanf("%d %d %d", &num1, &num2, &num3);
+    if(num1>num2 && num1>num3)
+        printf("%d is the largest number.", num1);
+    else if(num2>num1 && num2>num3)
+        printf("%d is the largest number.", num2);
+    else if(num3>num1 && num3>num2)
+        printf("%d is the largest number.", num3);
+        else
+        printf("All numbers are equal.");
+
+    return 0;
+}
