be30a4b7e6d5598d566bc88e44622f4307d216fb
Nabiha Tahsin <nabihatahsin688@gmail.com>
2026-08-08 20:25:22 +0600

    Refactor code structure for improved readability and maintainability

---
+++ vowel.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter a letter: ");
+    scanf("%c", &ch);
+    if(ch=='a')
+        printf("Vowel");
+    else if(ch=='e')
+        printf("Vowel");
+    else if(ch=='i')
+        printf("Vowel");
+    else if(ch=='o')
+        printf("Vowel");
+    else if(ch=='u')
+        printf("Vowel");
+    else
+        printf("Consonant");
+}
