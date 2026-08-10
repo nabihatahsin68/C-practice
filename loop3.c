bdd9aea2693870e82add23aec5203c9d6e118873
Nabiha Tahsin <nabihatahsin688@gmail.com>
2026-08-10 21:39:19 +0600

    Refactor code structure for improved readability and maintainability

---
+++ loop3.c
@@ -0,0 +1,12 @@
+//loop=for, while, do-while
+#include<stdio.h>
+int main()
+{
+    int i=1;//initialization
+    do
+    {
+        printf("%d\n", i);
+        i++;//increment
+    } while(i<=10);//condition
+    return 0;
+}
