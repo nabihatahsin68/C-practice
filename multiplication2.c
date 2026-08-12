07d5244577d3f3b0cae55894e443da524f78dfa8
Nabiha Tahsin <nabihatahsin688@gmail.com>
2026-08-12 14:29:45 +0600

    Refactor code structure for improved readability and maintainability

---
+++ multiplication2.c
@@ -0,0 +1,26 @@
+/* 
+Enter any number:3
+num x i=num*i
+3x1=3
+3x2=6
+
+
+..............
+3x10=30
+*/
+#include<stdio.h>
+int main()
+{
+    while(1)
+    {
+        int num,i;
+        printf("Enter any number:");
+        scanf("%d",&num);
+        for(i=1;i<=10;i++)
+        {
+            printf("%dx%d=%d\n",num,i,num*i);
+        }
+    }
+    return 0;
+}
+
