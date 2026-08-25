c2b1de0f495ac05d55350516f9b6d55276e8dee2
Nabiha Tahsin <nabihatahsin688@gmail.com>
2026-08-25 15:31:22 +0600

    Refactor code structure for improved readability and maintainability

---
+++ array8.c
@@ -0,0 +1,20 @@
+//write a program that cam take some numbers and display minimum
+#include<stdio.h>
+int main()
+{
+    int num[100] ,n,i;
+    printf("How many numbers: ");
+    scanf("%d" , &n);
+
+    for(i=0; i<n; i++)
+    { 
+        scanf("%d",&num[i]); 
+    }
+    int min=num[0];
+    for(i=1; i<n; i++ )
+    {
+        if(min> num[i])
+        min=num[i];
+    }
+    printf("Minimum=%d\n", min);
+}
